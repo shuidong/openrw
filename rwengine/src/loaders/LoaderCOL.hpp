8c04abce5cf6c9e783c7b50a31c398134c120b36
Daniel Evans <dan@rekt.io>
2016-09-03 02:59:15 +0100

    clang-format files in rwengine/src/loaders

--- rwengine/src/loaders/LoaderCOL.hpp
+++ rwengine/src/loaders/LoaderCOL.hpp
@@ -1,10 +1,10 @@
 #ifndef _LOADERCOL_H_
 #define _LOADERCOL_H_
+#include <data/CollisionModel.hpp>
+#include <glm/glm.hpp>
+#include <memory>
 #include <string>
 #include <vector>
-#include <memory>
-#include <glm/glm.hpp>
-#include <data/CollisionModel.hpp>
 
 /**
  * @class LoaderCOL
@@ -13,13 +13,13 @@
 class LoaderCOL
 {
 public:
-	/// Load the COL data into memory
-	bool load(const std::string& file);
-	
-	/// Load the COL data from a file already in memory
-	bool load(char* data, const size_t size);
-	
-	std::vector<std::unique_ptr<CollisionModel>> instances;
+  /// Load the COL data into memory
+  bool load(const std::string& file);
+
+  /// Load the COL data from a file already in memory
+  bool load(char* data, const size_t size);
+
+  std::vector<std::unique_ptr<CollisionModel>> instances;
 };
 
 #endif
