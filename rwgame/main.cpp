36c1cc591e81a434c354c2114056bbce03220a22
Daniel Evans <dan@rekt.io>
2016-09-03 02:59:15 +0100

    clang-format files in rwgame

--- rwgame/main.cpp
+++ rwgame/main.cpp
@@ -1,30 +1,30 @@
+#include <iostream>
 #include "RWGame.hpp"
 #include "SDL.h"
-#include <iostream>
 
 int main(int argc, char* argv[])
 {
-	try {
-		RWGame game(argc, argv);
+  try {
+    RWGame game(argc, argv);
 
-		return game.run();
-	} catch (std::runtime_error& ex) {
-		// Catch runtime_error as these are fatal issues the user may want to
-		// know about like corrupted files or GL initialisation failure.
-		// Catching other types (out_of_range, bad_alloc) would just make
-		// debugging them more difficult.
+    return game.run();
+  } catch (std::runtime_error& ex) {
+    // Catch runtime_error as these are fatal issues the user may want to
+    // know about like corrupted files or GL initialisation failure.
+    // Catching other types (out_of_range, bad_alloc) would just make
+    // debugging them more difficult.
 
-		const char* kErrorTitle = "Fatal Error";
+    const char* kErrorTitle = "Fatal Error";
 
-		std::cerr << kErrorTitle << "\n" << ex.what() << std::endl;
+    std::cerr << kErrorTitle << "\n" << ex.what() << std::endl;
 
-		if (SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, kErrorTitle,
-		                             ex.what(), NULL) < 0) {
-			SDL_Log("Failed to show message box\n");
-		}
+    if (SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, kErrorTitle, ex.what(), NULL) <
+        0) {
+      SDL_Log("Failed to show message box\n");
+    }
 
-		SDL_Quit();
+    SDL_Quit();
 
-		return -1;
-	}
+    return -1;
+  }
 }
